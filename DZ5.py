5b423bd0f13273633ec1aa81b7bdacfc8d38a2d9
«maleikovuch» <maleikovuch@mail.ru>
2022-11-26 00:28:06 +0300

    ДЗ 2-го семинара

--- DZ5.py
+++ DZ5.py
@@ -1,32 +1,23 @@
-# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+# Реализуйте алгоритм перемешивания списка.
+import random 
+from random import randint
 
-# Пример:
-
-# - A (3,6); B (2,1) -> 5,09
-# - A (7,-5); B (1,-1) -> 7,21
-
-from math import sqrt
-
-
-print('Введите координаты первой точки A: ')
-ax = float(input())
-ay = float(input())
-print('Введите координаты второй точки B:')
-bx = float(input())
-by = float(input())
-
-r = (bx-ax)**2 + (by-ay)**2
-rrr = sqrt (r)
-
-print(f'Расстояние между точками A и B -> {rrr} ')
+# list=[1,2,3,4,5,6]
+# print(list)
+# random.shuffle(list)
+# print(list)
 
 
-# r = round(((bx-ax)*(bx-ax) + (by-ay)*(by-ay))**0.5,3)
-# print(f'Расстояние между точками A и B -> {r} ')
+i=list(range(1,20))
+print(i)
+random.shuffle(i)
+print(i)
 
 
-# r = ((bx-ax)*(bx-ax) + (by-ay)*(by-ay))
-# rr = pow(r, 0.5)
-# print(f'Расстояние между точками A и B -> {rr} ')
-
+# list=[]
+# for i in range(20):
+#     list.append(randint(-10,10))
+# print(list)
+# random.shuffle(list)
+# print(list)
 
