2a9c8be69b2d0b04a8805d259329ba3c9554bdc4
«maleikovuch» <maleikovuch@mail.ru>
2022-11-29 21:19:53 +0300

    Добавил ДЗ семинара 3

--- DZ5.py
+++ DZ5.py
@@ -1,22 +1,35 @@
-# Реализуйте алгоритм перемешивания списка.
-import random
-from random import randint
+# 5'. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.(Дополнительно)
 
-# list=[1,2,3,4,5,6]
-# print(list)
-# random.shuffle(list)
-# print(list)
+# *Пример:*
 
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
 
-my_list = list(range(1, 20))
-print(f'Начальный список    ', my_list)
-random.shuffle(my_list)
-print(f'Перемешанный список ', my_list)
+n = int(input('Введите число: '))
 
-
+# def fib(n):
+#     if n in [1,2]:
+#         return 1
+#     else:
+#         return fib(n-2)+fib(n-1)                # фибоначчи
 # list=[]
-# for i in range(15):
-#     list.append(randint(-10,10))
+# for i in range(1,n+1):
+#     list.append(fib(i))
 # print(list)
-# random.shuffle(list)
-# print(list)
+
+# fib = lambda n: fib(n - 1) + fib(n - 2) if n > 2 else 1   # в одну строку поиск числа
+# print(fib(n))
+
+
+
+fib = []
+a, b = 1, 1
+for i in range(n):
+    fib.append(a)                                  # негафибоначчи
+    a, b = b, a + b
+
+a, b = 0, 1
+for i in range (n+1):
+    fib.insert(0, a)
+    a, b = b, a - b
+print(fib)
+
